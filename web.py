7f2c46804ceb41c0d0295ae0ce0cf562118446d3
何東霓 <dongniherh@gmail.com>
2025-03-20 23:40:44 +0800

    version 2: Python Web
    version 2: Python Web

--- web.py
+++ web.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello 我是東霓!"
+    homepage = "<h1>何東霓Python網頁</h1>"
+    homepage += "<a href=/mis>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=s1121427>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/account>網頁表單傳值</a><br>"
+    homepage += "<a href=/about>東霓簡介網頁</a><br>"
+    return homepage
+
 
 @app.route("/mis")
 def course():
@@ -14,6 +21,25 @@ def course():
 def today():
     now = datetime.now()
     return render_template("today.html", datetime = str(now))
+
+@app.route("/welcome", methods=["GET"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "；密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run()
